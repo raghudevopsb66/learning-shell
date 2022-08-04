d8cfc5812c3ca4a306f279d0f331f77abf335b5f
Raghu K <raghuk.vit@gmail.com>
2022-08-04 08:58:21 +0530

    Update learning-shell/

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,9 @@ echo Hello
 # Sometimes we need to print multi line
 
 echo -e "Hello\nWorld"
+# syntax :  -e is needed to print multi line/ infact to enable \n we need -e
+#           quotes are mandatory for using any \ esc seq , we used new line esc seq
+#           \n to print a new line
+
+# Tabbed line
+echo -e "one\t\ttwo"
