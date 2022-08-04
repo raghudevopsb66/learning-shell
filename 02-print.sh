50a390baedc3610501d73d6dd0d415bea4bbdfd0
Raghu K <raghuk.vit@gmail.com>
2022-08-04 09:01:55 +0530

    Update learning-shell/

--- 02-print.sh
+++ 02-print.sh
@@ -12,9 +12,21 @@ echo -e "one\t\ttwo"
 
 # Color printing
 echo -e "\e[31mHello\e[0m"
+echo -e "\e[32mHello\e[0m"
+echo -e "\e[33mHello\e[0m"
+echo -e "\e[34mHello\e[0m"
+echo -e "\e[35mHello\e[0m"
+echo -e "\e[36mHello\e[0m"
 
 # Syntax:   -e to enable \e
 #           \e[31m, 31 is the color code
 #           \e[0m , 0 is to reset the color
 ## Note : In shell when we enable any color and it is our responsibility to disable it.
 
+## Colors   # CODE
+# RED         31
+# GREEN       32
+# YELLOW      33
+# BLUE        34
+# MAGENTA     35
+# CYAN        36
