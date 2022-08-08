72029ea3de44f2b988d6b5c1dad148478de0e563
Raghu K <raghuk.vit@gmail.com>
2022-08-08 08:02:11 +0530

    Update learning-shell/

--- 04-if.sh
+++ 04-if.sh
@@ -7,3 +7,6 @@ else
   echo Both are not same
 fi
 
+## If you are using variables in expressions the always double quote them
+# Meaning
+# [ $x = abc ] is better to keep quotes as [ "$x" = abc ] to avoid unnecessary errors
