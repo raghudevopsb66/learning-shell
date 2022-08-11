c209a823912311447e92cfb1a529fbcc465150da
Raghu K <raghuk.vit@gmail.com>
2022-08-11 07:48:51 +0530

    Update learning-shell/

--- 07-inputs.sh
+++ 07-inputs.sh
@@ -11,3 +11,11 @@ echo Second Argument = $2
 echo All Arguments = $*
 echo Number of Arguments = $#
 
+SAMPLE() {
+  echo First Argument in Function = $1
+  echo Second  Argument in Function = $2
+  echo All Arguments in Function = $*
+  echo Number of Arguments = $#
+}
+
+SAMPLE 100 200
